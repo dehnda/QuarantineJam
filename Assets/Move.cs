51a0cb56e42e116febceb881cc43e7b0db567e92
Daniel Nimmervoll <daniel.nimmervoll@gmail.com>
2020-03-13 21:40:14 +0100

    create tilemap for environment

---
+++ Assets/Move.cs
@@ -0,0 +1,37 @@
+﻿// Move sprite bottom left to upper right.  It does not stop moving.
+// The Rigidbody2D gives the position for the cube.
+
+using UnityEngine;
+using Unity;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    // public Texture2D tex;
+
+    [SerializeField]
+    private float speed = 5f;
+    private Vector2 velocity;
+    private Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        velocity = new Vector2(h, 0f);
+
+        velocity *= speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Sprites/Paper-Pixels/README.md
@@ -0,0 +1,7 @@
+# Paper Pixels
+
+Enjoy my assets? Keep up to date by following me on itch.
+https://v3x3d.itch.io/
+
+You can also support me on patreon for just a $1 a month.
+https://www.patreon.com/V3X3D
