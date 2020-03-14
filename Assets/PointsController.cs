4711a63f1d25a27a958bda6ab292069e25545ff9
Daniel Nimmervoll <daniel.nimmervoll@gmail.com>
2020-03-14 17:26:44 +0100

    point sytem with eat and float items

---
+++ Assets/PointsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsController : MonoBehaviour
+{
+    [SerializeField]
+    private float startPoints = 100f;
+    [SerializeField]
+    private float curentPoints = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddPoints(float points)
+    {
+        curentPoints += points;
+        curentPoints = Mathf.Min(curentPoints, startPoints);
+    }
+}
