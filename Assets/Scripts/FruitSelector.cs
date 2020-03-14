a14290dd4c049a3f07c7ab23b54d566bacebf4de
Daniel Nimmervoll <daniel.nimmervoll@gmail.com>
2020-03-14 15:07:56 +0100

    controlelr for fruit and junk spawn and added bubble sprites

---
+++ Assets/Scripts/FruitSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FruitSelector : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] sprites = new Sprite[9];
+    private SpriteRenderer spriteRenderer = null;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[Random.Range(0, 9)];
+    }
+}
---
+++ Assets/SpawnController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fruitPrefab = null;
+    [SerializeField]
+    private GameObject junkPrefab = null;
+    [SerializeField]
+    private float timeToNextSpawn = 5f;
+    [SerializeField]
+    bool GameIsRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameIsRunning = true;
+        StartCoroutine("InstantiateFruitRoutine");
+        StartCoroutine("InstantiateJunkRoutine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator InstantiateJunkRoutine()
+    {
+        while (GameIsRunning)
+        {
+
+            yield return new WaitForSeconds(timeToNextSpawn);
+            var position = new Vector3(Random.Range(-8f, 9f), 5f);
+            Instantiate(junkPrefab, position, Quaternion.identity);
+        }
+    }
+
+    IEnumerator InstantiateFruitRoutine()
+    {
+        while (GameIsRunning)
+        {
+
+            yield return new WaitForSeconds(timeToNextSpawn);
+            var position = new Vector3(Random.Range(-8f, 9f), 5f);
+            Instantiate(fruitPrefab, position, Quaternion.identity);
+        }
+    }
+}
