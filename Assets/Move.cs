b23d5235576abe7abbe017ed145f854cc8d5c732
Daniel Nimmervoll <daniel.nimmervoll@gmail.com>
2020-03-14 11:39:14 +0100

    add jump feature for player

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,37 +1,64 @@
-﻿// Move sprite bottom left to upper right.  It does not stop moving.
-// The Rigidbody2D gives the position for the cube.
-
+﻿using Unity;
 using UnityEngine;
-using Unity;
-using System.Collections;
+
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
 public class Move : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 5f;
+    private float jumpForce = 400f;
+    [SerializeField]
+    private float speed = 8f;
+    [SerializeField]
     private Vector2 velocity;
     private Rigidbody2D rb2D;
+    private BoxCollider2D boxCollider = null;
+    private float distToGround = 0f;
+    bool jump = false;
 
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        // get the distance to ground
+        distToGround = boxCollider.bounds.extents.y;
     }
 
     void Update()
     {
-
         var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
 
         velocity = new Vector2(h, 0f);
 
         velocity *= speed;
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            jump = true;
+            Debug.Log("Jump");
+        }
+    }
+
+    bool IsGrounded()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        return rb2D.IsTouchingLayers(layerMask);
+
     }
 
     void FixedUpdate()
     {
-        rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+
+        if (jump)
+        {
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+            jump = false;
+        }
+
+        rb2D.velocity = new Vector2(velocity.x, rb2D.velocity.y);
     }
 }
