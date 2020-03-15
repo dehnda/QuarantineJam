aa0d3beba6a4bdbf04ccadc0ea617112d1ef18a4
Daniel Nimmervoll <daniel.nimmervoll@gmail.com>
2020-03-15 09:48:28 +0100

    particle effect on bubblenator

--- Assets/BubbleNator.cs
+++ Assets/BubbleNator.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class BubbleNator : MonoBehaviour
 {
+    [SerializeField]
+    private ParticleSystem particle = null;
     private CircleCollider2D circle = null;
     // Start is called before the first frame update
     private MovementItems target = null;
+
+    void Awake()
+    {
+        particle = particle.GetComponent<ParticleSystem>();
+        particle.Stop();
+
+    }
     void Start()
     {
         circle = GetComponent<CircleCollider2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1") && (target != null))
+
+        if (Input.GetButton("Fire1"))
         {
-            target.SetFloatingTarget();
-            target = null;
+            FireBubbleParticles();
+            if (target != null)
+            {
+
+                target.SetFloatingTarget();
+                target = null;
+            }
         }
 
         if (Input.GetButton("Fire2") && (target != null))
@@ -27,6 +43,13 @@ public class BubbleNator : MonoBehaviour
             target.SetEatingTarget();
             target = null;
         }
+    }
+
+    private void FireBubbleParticles()
+    {
+        //particle.Play();
+        particle.Emit(transform.position, )
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
