48fcefb9508851ab0a31717735a8ce384e1ee356
Daniel Nimmervoll <daniel.nimmervoll@gmail.com>
2020-03-16 14:54:05 +0100

    Fade in out to game

---
+++ Assets/FadeIn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField]
+    private Image img = null;
+
+    void Start()
+    {
+        StartCoroutine(FadeImage());
+    }
+
+    IEnumerator FadeImage()
+    {
+        // fade from opaque to transparent
+
+        // loop over 1 second backwards
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            img.color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+        FindObjectOfType<GameManager>().EnterGame();
+
+    }
+
+}
---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField]
+    private Image img = null;
+
+    [SerializeField]
+    private float waitTime = 2f;
+    [SerializeField]
+    private string sceneName = "SceneToLoad";
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeImage(true));
+    }
+
+
+    IEnumerator ChangeSceneRoutine(float wait)
+    {
+
+        yield return new WaitForSeconds(wait);
+        StartCoroutine(FadeImage(false));
+    }
+
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+            StartCoroutine(ChangeSceneRoutine(waitTime));
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+}
